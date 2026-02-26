288c537bee97dc08f986f952461b9524db91fe37
Anurag <apatel665@gmail.com>
2026-02-26 13:33:12 +0530

    logistic model ready

--- linear.py
+++ linear.py
@@ -2,32 +2,33 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
 np.random.seed(42)
 
 # Generate 100 samples
 n_samples = 100
-
 data = {
     
     'hours_study': np.random.randint(3, 8, n_samples),
     'exam_score': np.random.uniform(50, 95, n_samples),
-    
-}
+    }
 df = pd.DataFrame(data)
 df.to_csv('data/score_data.csv', index=False)
 print(f"Generated {len(df)} samples")
 
 # read the data
 df = pd.read_csv('data/score_data.csv')
+
 #extract features and target variable
 X = df[['hours_study']]
 y = df['exam_score']
+
 #split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 #train the linear regression model
 model = LinearRegression()
 model.fit(X_train, y_train)
+
 #take user input for make predictions
 hours = float(input("Enter the number of hours studied: "))
 prediction = model.predict([[hours]])
---
+++ logistic.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+np.random.seed(42)
+
+# Generate 500 samples
+n_samples = 500
+data = {
+    'age': np.random.randint(18, 60, n_samples),
+    'timespent': np.random.randint(30, 60, n_samples),
+    'addtocart': np.random.poisson(lam=1, size=n_samples)
+}
+
+# Optional: Create sentiment score (0 = Negative, 1 = Positive)
+sentiment = np.where(data['addtocart'] > 1, 1, 0)
+df = pd.DataFrame(data)
+df['sentiment'] = sentiment
+print(df.head())
+df.to_csv('data/score_data.csv', index=False)
+print(f"Generated {len(df)} samples")
+
+# read the data
+df = pd.read_csv('data/score_data.csv')
+
+# extract features and target variable
+X = df[['age', 'timespent', 'addtocart']]
+y = df['sentiment']
+
+#split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#train the linear regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+#check the accuracy of the model
+accuracy = model.score(X_test, y_test)
+print(f"Model Accuracy: {accuracy:.2f}")
+
+#take user input for make predictions
+user_age = float(input("Enter the age: "))
+user_timespent = float(input("Enter the time spent: "))
+user_addtocart = float(input("Enter the number of items added to cart: "))
+
+#input set into array and make prediction
+user_data=np.array([[user_age, user_timespent, user_addtocart]])
+
+prediction = model.predict(user_data)
+if prediction[0] == 1:
+    print("likely to purchase")
+else:    print("not likely to purchase")
+
+    
